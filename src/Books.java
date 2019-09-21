6e154e1822867dc0ad9ffe5e1f1dd52e4600bed2
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-09-21 23:54:39 +0530

    final commit

--- src/Books.java
+++ src/Books.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class Books {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Books01[] book = new Books01[n];
+        for (int i = 0; i < n; i++) {
+            String bookName = scanner.nextLine().trim();
+            String authorName = scanner.nextLine().trim();
+            int ISBNNumber = scanner.nextInt();
+            book[i] = new Books01(bookName, authorName, ISBNNumber);
+        }
+        for (int j = 0; j < n; j++) {
+            System.out.print("-----------------------------");
+            System.out.print(book[j]);
+        }
+    }
 }
 class Books01{
     private String bookName;
@@ -27,5 +44,17 @@ class Books01{
 
     public void setISBNNumber(long ISBNNumber) {
         this.ISBNNumber = ISBNNumber;
+    }
+
+    public Books01(String bookName, String authorName, long ISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBNNumber = ISBNNumber;
+    }
+
+    @Override
+    public String toString() {
+        return String.
+                format(getBookName(), getAuthorName(), getISBNNumber());
     }
 }
