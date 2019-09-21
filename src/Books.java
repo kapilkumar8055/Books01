febef2ea6e51835d63112403e6c0b1f2efb4e9ed
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-09-21 22:29:50 +0530

    create getter setter for bookName,authorName,ISBNNumber

--- src/Books.java
+++ src/Books.java
@@ -1,5 +1,31 @@
 public class Books {
 }
 class Books01{
+    private String bookName;
+    private String authorName;
+    private long ISBNNumber;
 
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public long getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public void setISBNNumber(long ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
 }
